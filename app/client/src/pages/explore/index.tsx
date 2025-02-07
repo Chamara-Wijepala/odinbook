389271bf8909f32cf0c5a9f20e03c50fa80adc75
Chamara-Wijepala <mr.worf777@gmail.com>
2025-02-07 07:18:03 +0530

    refactor: always display explore page header

--- app/client/src/pages/explore/index.tsx
+++ app/client/src/pages/explore/index.tsx
@@ -4,7 +4,7 @@ import api from '../../api';
 import type { PostType } from '../../types';
 
 export default function Explore() {
-	const [posts, setPosts] = useState<PostType[] | null>(null);
+	const [posts, setPosts] = useState<PostType[]>([]);
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
@@ -19,33 +19,30 @@ export default function Explore() {
 		})();
 	}, []);
 
-	if (isLoading) {
-		return (
-			<div className="p-4 flex flex-col gap-4">
-				<PostSkeleton />
-				<PostSkeleton />
-				<PostSkeleton />
-				<PostSkeleton />
-				<PostSkeleton />
-			</div>
-		);
-	}
-
-	if (posts && posts.length < 1) {
-		return (
-			<div className="text-center p-4 pt-8">
-				<p className="text-lg text-slate-500">
-					Looks like there's nothing here. Be the first to create a post!
-				</p>
-			</div>
-		);
-	}
-
 	return (
 		<div className="p-4">
 			<h2 className="text-slate-700 dark:text-slate-300 text-lg font-semibold my-4">
 				See what people are saying
 			</h2>
+
+			{posts.length < 1 && !isLoading && (
+				<div className="text-center p-4 pt-8">
+					<p className="text-lg text-slate-500">
+						Looks like there's nothing here. Be the first to create a post!
+					</p>
+				</div>
+			)}
+
+			{posts.length < 1 && isLoading && (
+				<div className="p-4 flex flex-col gap-4">
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+					<PostSkeleton />
+				</div>
+			)}
+
 			<div className="flex flex-col gap-4">
 				{posts?.map((post) => (
 					<Post
