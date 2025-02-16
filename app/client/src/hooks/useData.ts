1342b87150652a3304de2ca911d8a5c363aa344f
Chamara-Wijepala <mr.worf777@gmail.com>
2025-02-16 10:25:50 +0530

    refactor: move data fetching functionality to hook

---
+++ app/client/src/hooks/useData.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react';
+import api from '../api';
+
+export default function useData<T>(url: string) {
+	const [isLoading, setIsLoading] = useState(true);
+	const [data, setData] = useState<T | null>(null);
+	const [error, setError] = useState<unknown | null>(null);
+
+	useEffect(() => {
+		(async () => {
+			try {
+				const response = await api.get(url);
+				setData(response.data);
+			} catch (err) {
+				setError(err);
+			} finally {
+				setIsLoading(false);
+			}
+		})();
+	}, []);
+
+	return {
+		isLoading,
+		data,
+		error,
+	};
+}
--- app/client/src/pages/explore/index.tsx
+++ app/client/src/pages/explore/index.tsx
@@ -1,23 +1,10 @@
-import { useState, useEffect } from 'react';
 import Post, { PostSkeleton } from '../../components/post';
-import api from '../../api';
+import useData from '../../hooks/useData';
 import type { PostType } from '../../types';
 
 export default function Explore() {
-	const [posts, setPosts] = useState<PostType[]>([]);
-	const [isLoading, setIsLoading] = useState(true);
-
-	useEffect(() => {
-		(async () => {
-			try {
-				const response = await api.get('/posts/explore');
-				setPosts(response.data);
-				setIsLoading(false);
-			} catch (error) {
-				console.log(error);
-			}
-		})();
-	}, []);
+	// Error isn't needed because endpoint will always return an array.
+	const { isLoading, data: posts } = useData<PostType[]>('/posts/explore');
 
 	return (
 		<div className="p-4">
@@ -25,21 +12,21 @@ export default function Explore() {
 				See what people are saying
 			</h2>
 
-			{posts.length < 1 && !isLoading && (
-				<div className="text-center p-4 pt-8">
-					<p className="text-lg text-slate-500">
-						Looks like there's nothing here. Be the first to create a post!
-					</p>
-				</div>
-			)}
-
-			{posts.length < 1 && isLoading && (
+			{isLoading && (
 				<div className="p-4 flex flex-col gap-4">
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
 					<PostSkeleton />
+				</div>
+			)}
+
+			{posts && posts.length < 1 && !isLoading && (
+				<div className="text-center p-4 pt-8">
+					<p className="text-lg text-slate-500">
+						Looks like there's nothing here. Be the first to create a post!
+					</p>
 				</div>
 			)}
 
