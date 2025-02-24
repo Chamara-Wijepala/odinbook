0f140d4f4ca8b21437e0a1e461f5ed1102f7c051
Chamara-Wijepala <mr.worf777@gmail.com>
2025-02-24 15:43:01 +0530

    fix: content being centered vertically on page

--- app/client/src/ui/navigation-layout/index.tsx
+++ app/client/src/ui/navigation-layout/index.tsx
@@ -3,7 +3,7 @@ import Navbar from '../navbar';
 
 export default function NavigationLayout() {
 	return (
-		<div className="grid lg:grid-cols-[300px_1fr_300px] min-h-[100svh] max-w-7xl mx-auto">
+		<div className="lg:grid lg:grid-cols-[300px_1fr_300px] min-h-[100svh] max-w-7xl mx-auto">
 			<Navbar />
 
 			<div className="lg:border-x-[1px] border-slate-300 dark:border-slate-800 bg-slate-50 dark:bg-slate-950 w-full">
