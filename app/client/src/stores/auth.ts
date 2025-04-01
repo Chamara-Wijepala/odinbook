3ad8d8542a5df9650a26f2939e86f3d0c1690f96
Chamara-Wijepala <mr.worf777@gmail.com>
2025-04-01 10:51:36 +0530

    refactor: add name and enabled status to auth store

--- app/client/src/stores/auth.ts
+++ app/client/src/stores/auth.ts
@@ -1,6 +1,8 @@
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
+const enabled = import.meta.env.MODE === 'production' ? false : true;
+
 type User = {
 	id: string;
 	firstName: string;
@@ -20,37 +22,40 @@ type AuthStore = {
 };
 
 const useAuthStore = create(
-	devtools<AuthStore>((set) => ({
-		token: null,
-		user: null,
-		setToken: (token: string) => set(() => ({ token })),
-		clearToken: () => set(() => ({ token: null })),
-		setUser: (user: User) => set(() => ({ user })),
-		updateUserFollowing: (id) =>
-			set((state) => {
-				if (!state.user) return state;
+	devtools<AuthStore>(
+		(set) => ({
+			token: null,
+			user: null,
+			setToken: (token: string) => set(() => ({ token })),
+			clearToken: () => set(() => ({ token: null })),
+			setUser: (user: User) => set(() => ({ user })),
+			updateUserFollowing: (id) =>
+				set((state) => {
+					if (!state.user) return state;
 
-				return {
-					...state,
-					user: {
-						...state.user,
-						following: [...state.user?.following, id],
-					},
-				};
-			}),
-		deleteUserFollowing: (idToRemove) =>
-			set((state) => {
-				if (!state.user) return state;
+					return {
+						...state,
+						user: {
+							...state.user,
+							following: [...state.user?.following, id],
+						},
+					};
+				}),
+			deleteUserFollowing: (idToRemove) =>
+				set((state) => {
+					if (!state.user) return state;
 
-				return {
-					...state,
-					user: {
-						...state.user,
-						following: state.user.following.filter((id) => id !== idToRemove),
-					},
-				};
-			}),
-	}))
+					return {
+						...state,
+						user: {
+							...state.user,
+							following: state.user.following.filter((id) => id !== idToRemove),
+						},
+					};
+				}),
+		}),
+		{ name: 'Auth Store', enabled }
+	)
 );
 
 export default useAuthStore;
