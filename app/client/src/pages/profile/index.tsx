12645ab3f6088bb5944ca2b17255fdfb84b09c63
Chamara-Wijepala <mr.worf777@gmail.com>
2025-04-24 09:47:01 +0530

    feat: update profile skeleton

--- app/client/src/pages/profile/index.tsx
+++ app/client/src/pages/profile/index.tsx
@@ -301,11 +301,16 @@ export default function Profile() {
 }
 
 function Skeleton() {
+	const params = useParams();
+	const currentUser = useAuthStore((s) => s.user);
+
 	return (
 		<div className="p-4 animate-pulse">
-			<div className="bg-slate-200 dark:bg-slate-700 rounded-full w-[80px] sm:w-[100px] aspect-square"></div>
+			{/* avatar */}
+			<div className="bg-slate-200 dark:bg-slate-700 rounded-full w-[80px] sm:w-[120px] aspect-square"></div>
 
 			<div className="my-6 flex gap-x-1 items-start">
+				{/* names and username */}
 				<div>
 					<div className="flex gap-x-1 mb-2">
 						<div className="bg-slate-200 dark:bg-slate-700 rounded-md h-4 w-20"></div>
@@ -315,11 +320,16 @@ function Skeleton() {
 					<div className="bg-slate-200 dark:bg-slate-700 rounded-md h-4 w-32"></div>
 				</div>
 
-				<div className="ml-auto bg-slate-200 dark:bg-slate-700 h-8 w-20 rounded-full"></div>
+				{/* follow/unfollow button */}
+				{currentUser?.username !== params.username && (
+					<div className="ml-auto bg-slate-200 dark:bg-slate-700 h-8 w-20 rounded-full"></div>
+				)}
 			</div>
 
+			{/* join date */}
 			<div className="my-6 bg-slate-200 dark:bg-slate-700 h-4 w-32 rounded-md"></div>
 
+			{/* followers and post count */}
 			<div className="flex gap-x-4 items-center">
 				<div className="bg-slate-200 dark:bg-slate-700 h-4 w-16 rounded-md"></div>
 				<div className="bg-slate-200 dark:bg-slate-700 h-4 w-16 rounded-md"></div>
