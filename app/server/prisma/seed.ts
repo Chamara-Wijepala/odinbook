2853fad710400dcfc2ff08de3430b96a4a81903b
Chamara-Wijepala <mr.worf777@gmail.com>
2025-05-18 14:07:48 +0530

    revert: "feat: seed db after server installation"
    This reverts commit ef1ba0339580eab28433ff5a82a2f056454424f4.

--- app/server/prisma/seed.ts
+++ app/server/prisma/seed.ts
@@ -2,12 +2,6 @@ import bcrypt from 'bcryptjs';
 import prisma from '../db/prisma';
 
 async function main() {
-	const user = await prisma.user.findUnique({
-		where: { username: 'JohnDoe1' },
-	});
-
-	if (user) return;
-
 	const hash = await bcrypt.hash('helloworld', 10);
 	await prisma.user.create({
 		data: {
