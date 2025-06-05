9e9acbca599bf6abfc80454021fdab1fe7f100a3
Chamara-Wijepala <mr.worf777@gmail.com>
2025-06-05 10:34:09 +0530

    fix: build fail from unused import

--- app/server/integration/users/profile.test.ts
+++ app/server/integration/users/profile.test.ts
@@ -1,4 +1,4 @@
-import { describe, test, expect, expectTypeOf, beforeAll } from 'vitest';
+import { describe, test, expect, beforeAll } from 'vitest';
 import request from 'supertest';
 import app from '../../app';
 import { userData, getUserId, getAccessToken } from '../common';
