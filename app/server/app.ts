79bda308f6a6d5e01230619dca488890024df6a3
Chamara-Wijepala <mr.worf777@gmail.com>
2025-06-10 12:00:19 +0530

    feat: add endpoint for render.com health checks

--- app/server/app.ts
+++ app/server/app.ts
@@ -20,6 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.get('/', (req, res) => {
+	res.status(200).send('Server is alive!');
+});
 app.get('/ping', (req, res) => {
 	res.status(200).send('Server is alive!');
 });
